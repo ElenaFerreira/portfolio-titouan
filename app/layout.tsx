d9d3918d201b47563316983295c3dcc9329c19b4
DumbWays31 <elena.ferreira@free.fr>
2025-11-28 16:48:13 +0100

    feat: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,14 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr">
+      <head>
+        <link rel="icon" type="image/png" href="/images/favicon/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/images/favicon/favicon.svg" />
+        <link rel="shortcut icon" href="/images/favicon/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="Titouan - Portfolio" />
+        <link rel="manifest" href="/images/favicon/site.webmanifest" />
+      </head>
       <body>{children}</body>
     </html>
   );
